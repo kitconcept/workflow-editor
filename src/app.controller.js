fb4c47c6a942613bd6cb82ae7dcfe3910892ea8e
Timo Stollenwerk <tisto@plone.org>
2016-08-04 16:43:16 +0200

    Fix draggable by making elements draggable after all endpoints and connections have been added.

--- src/app.controller.js
+++ src/app.controller.js
@@ -16,6 +16,7 @@ class AppController {
     this.flowchart = flowchart.get();
     var instance = this.flowchart.instance;
     var _addEndpoints = function(toId) {
+      console.log('_addEndpoint');
       var anchors = ['TopCenter', 'BottomCenter', 'LeftMiddle', 'RightMiddle'];
       for (var i = 0; i < anchors.length; i++) {
         var sourceUUID = toId + anchors[i];
@@ -31,14 +32,16 @@ class AppController {
     };
 
     jsPlumb.ready(function() {
+      console.log('jsPlumb ready');
+      console.log('jsPlumb instance (ready):' + instance);
       instance.doWhileSuspended(function() {
-        instance.draggable($('.node'), { grid: [20, 20] });
         _addEndpoints('container0');
         _addEndpoints('container1');
         _addEndpoints('container2');
         instance.connect({uuids:['container0RightMiddle', 'container1LeftMiddle'], editable:true});
         instance.connect({uuids:['container1BottomCenter', 'container2RightMiddle'], editable:true});
         instance.connect({uuids:['container2LeftMiddle', 'container0BottomCenter'], editable:true});
+        instance.draggable($('.node'), { grid: [20, 20] });
       });
     });
   }
--- src/app.js
+++ src/app.js
@@ -5,7 +5,17 @@ require('jquery');
 require('jqueryui');
 require('./../jsPlumb-2.2.0.js');
 
-angular.module('workflowEditor', []).directive('app', AppComponent).run(function(flowchart) {
+angular.module('workflowEditor', [])
+.directive('app', AppComponent)
+.factory('flowchart', function() {
+  var flowchart = null;
+
+  return {
+    get: function() { return flowchart; },
+    set: function(fc) { flowchart = fc; }
+  };
+})
+.run(function(flowchart) {
   var instance = jsPlumb.getInstance({
     DragOptions : { cursor: 'pointer', zIndex:2000 },
     ConnectionOverlays : [
@@ -20,38 +30,32 @@ angular.module('workflowEditor', []).directive('app', AppComponent).run(function
     ],
     Container:'flowchart'
   });
+  console.log('jsPlumb instance: ' + instance);
   flowchart.set({
-      'id': 1,
-      'instance': instance,
-      'nodes': [
-        {
-          'id': 0,
-          'title': 'Public',
-          'text': 'Visible for everybody',
-          'top': 200,
-          'left': 10,
-        },
-        {
-          'id': 1,
-          'title': 'Private',
-          'text': 'Internally visible only.',
-          'top': 0,
-          'left': 500,
-        },
-        {
-          'id': 2,
-          'title': 'Pending',
-          'text': 'Pending for review.',
-          'top': 340,
-          'left': 420,
-        },
-      ]
-    });
-}).factory('flowchart', function() {
-  var flowchart = null;
-
-  return {
-    get: function() { return flowchart; },
-    set: function(fc) { flowchart = fc; }
-  };
+    'id': 1,
+    'instance': instance,
+    'nodes': [
+      {
+        'id': 0,
+        'title': 'Public',
+        'text': 'Visible for everybody',
+        'top': 200,
+        'left': 10,
+      },
+      {
+        'id': 1,
+        'title': 'Private',
+        'text': 'Internally visible only.',
+        'top': 0,
+        'left': 500,
+      },
+      {
+        'id': 2,
+        'title': 'Pending',
+        'text': 'Pending for review.',
+        'top': 340,
+        'left': 420,
+      },
+    ]
+  });
 });
