76252a5d71b290c00a02d84f6bd4aa1d732cd592
Timo Stollenwerk <tisto@plone.org>
2016-08-04 15:11:05 +0200

    Add jquery, jqueryui and jsplumb example.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,17 @@
 import angular from 'angular';
 import AppComponent from './app.component.js';
 
-angular.module('workflowEditor', []).directive('app', AppComponent);
+require('jquery');
+require('jqueryui');
+require('./../jsPlumb-2.2.0.js');
+
+angular.module('workflowEditor', []).directive('app', AppComponent).run(
+  jsPlumb.ready(function() {
+    jsPlumb.connect({
+      source:"item_left",
+      target:"item_right",
+      endpoint:"Rectangle"
+    })
+  })
+);
+
