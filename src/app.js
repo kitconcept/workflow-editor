492d5b80026f25897a9182b1b32fc05b26410270
Timo Stollenwerk <tisto@plone.org>
2016-08-11 15:50:15 +0200

    Use proper es6 imports for jquery, jqueryui and jsPlumb.

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ import angular from 'angular';
 import AppComponent from './app.component.js';
 import WorkflowService from './app.service.js';
 
-require('jquery');
-require('jqueryui');
-require('./../jsPlumb-2.2.0.js');
+import $ from 'jquery';
+import 'jqueryui';
+import jsPlumb from './../jsPlumb-2.2.0.js';
 
 angular.module('workflowEditor', [])
 .directive('app', AppComponent)
