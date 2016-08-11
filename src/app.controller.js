4ab9e8d92ba8a5c1ef0063c0f142b0337af488f5
Timo Stollenwerk <tisto@plone.org>
2016-08-11 13:52:59 +0200

    Set endpoint radius to 5px; Add (commented out manually set anchors.

--- src/app.controller.js
+++ src/app.controller.js
@@ -1,10 +1,10 @@
  // import WorkflowService from './app.service.js'
 
  var flowchartEndpointOptions = {
-    endpoint:'Dot',
-    isSource:true,
-    isTarget:true,
-    maxConnections:-1,
+    endpoint: ['Dot', {radius: 5} ],
+    isSource: true,
+    isTarget: true,
+    maxConnections: -1,
     connector: [
       'Flowchart',
       { stub:[40, 60], gap:10, cornerRadius:5, alwaysRespectStubs:true }
@@ -20,10 +20,11 @@ class AppController {
     var instance = jsPlumb;
     var _addEndpoints = function(toId) {
       var anchors = [
-        'TopCenter',
-        'BottomCenter',
-        'LeftMiddle',
-        'RightMiddle'
+        // [dX, dY, anchorOrientationX, anchorOrientationY]
+        'TopCenter', // [0.5, 0, 0, 1, 'TopCenter'], // Top Center
+        'BottomCenter', // [0.5, 1, 0, 0, 'BottomCenter'], // Bottom Center
+        'LeftMiddle', // [0, 0.5, 1, 1, 'LeftMiddle'],  // Left Middle
+        'RightMiddle', // [1, 0.5, 1, 0, 'RightMiddle'] // Right Middle
       ];
       for (var i = 0; i < anchors.length; i++) {
         var sourceUUID = toId + anchors[i];
