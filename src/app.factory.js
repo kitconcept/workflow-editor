f955aaa29d2af183b4e2a1b69d1a78f13221fa71
Timo Stollenwerk <tisto@plone.org>
2016-08-11 13:56:02 +0200

    Remove flowchart factory.

--- src/app.factory.js
+++
@@ -1,9 +0,0 @@
-let workflowFactory = () => {
-  var flowchart = null;
-  return {
-    get: function() { return flowchart; },
-    set: function(fc) { flowchart = fc; }
-  };
-};
-
-export default workflowFactory;
--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import angular from 'angular';
 import AppComponent from './app.component.js';
-import WorkflowFactory from './app.factory.js';
 import WorkflowService from './app.service.js';
 
 require('jquery');
@@ -10,8 +9,7 @@ require('./../jsPlumb-2.2.0.js');
 angular.module('workflowEditor', [])
 .directive('app', AppComponent)
 .service('workflowService', WorkflowService)
-.factory('flowchart', WorkflowFactory)
-.run(function(flowchart) {
+.run(function() {
   var instance = jsPlumb.getInstance({
     DragOptions : { cursor: 'pointer', zIndex:2000 },
     ConnectionOverlays : [
@@ -25,36 +23,5 @@ angular.module('workflowEditor', [])
       ]
     ],
     Container:'flowchart'
-  });
-  flowchart.set({
-    'id': 1,
-    'instance': instance,
-    'nodes': [
-      {
-        'id': 0,
-        'title': 'Public',
-        'text': 'Visible for everybody',
-        'top': 200,
-        'left': 10,
-      },
-      {
-        'id': 1,
-        'title': 'Private',
-        'text': 'Internally visible only.',
-        'top': 0,
-        'left': 500,
-      },
-      {
-        'id': 2,
-        'title': 'Pending',
-        'text': 'Pending for review.',
-        'top': 340,
-        'left': 420,
-      },
-    ],
-    'transactions': [
-      {'from': 0, 'to': 1},
-      {'from': 1, 'to': 2}
-    ]
   });
 });
