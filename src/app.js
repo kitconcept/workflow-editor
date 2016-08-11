9fbf6e27f8926bed05793987f8ca35c7c3ff6fc8
Timo Stollenwerk <tisto@plone.org>
2016-08-11 16:35:41 +0200

    Move styles from html to separate css file.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import AppComponent from './app.component.js';
 import WorkflowService from './app.service.js';
+require('./styles.css');
 
 import $ from 'jquery';
 import 'jqueryui';
