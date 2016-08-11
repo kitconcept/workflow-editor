4cd6a8d971d3fe5bc02eb97ae44d25147dc616dc
Timo Stollenwerk <tisto@plone.org>
2016-08-11 20:00:46 +0200

    Add test for states.

--- spec.js
+++ spec.js
@@ -1,9 +1,16 @@
 // spec.js
 describe('Worklow Editor', function() {
+
   it('should show headline', function() {
     browser.get('http://localhost:8080');
 
     expect(element(by.css('h1')).getText()).
       toEqual('Workflow Example');
   });
+
+  it('should contain all four states', function() {
+    browser.get('http://localhost:8080');
+    expect(element.all(by.css('.state')).count()).toEqual(4);
+  });
+
 });
