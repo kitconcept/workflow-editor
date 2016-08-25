f7b3c0dd22ebf615eff34e3c88ee848d10a10550
Timo Stollenwerk <tisto@plone.org>
2016-08-25 12:10:12 +0200

    Use double quotes in spec.js.

--- spec.js
+++ spec.js
@@ -1,23 +1,23 @@
 // spec.js
-describe('Worklow Editor', function() {
+describe("Worklow Editor", function() {
 
-  it('should show headline', function() {
-    browser.get('http://localhost:8080');
+  it("should show headline", function() {
+    browser.get("http://localhost:8080");
 
-    expect(element(by.css('h1')).getText()).
-      toEqual('Workflow Example');
+    expect(element(by.css("h1")).getText()).
+      toEqual("Workflow Example");
   });
 
-  it('should contain all four states', function() {
-    browser.get('http://localhost:8080');
-    expect(element.all(by.css('.state')).count()).toEqual(4);
+  it("should contain all four states", function() {
+    browser.get("http://localhost:8080");
+    expect(element.all(by.css(".state")).count()).toEqual(4);
   });
 
 /*
-  it('should contain all four transitions', function() {
-    browser.get('http://localhost:8080');
+  it("should contain all four transitions", function() {
+    browser.get("http://localhost:8080");
     browser.pause()
-    expect(element.all(by.css('.jsplumb-connector')).count()).toEqual(4);
+    expect(element.all(by.css(".jsplumb-connector")).count()).toEqual(4);
   });
 */
 
