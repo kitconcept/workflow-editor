904a2ec03855e0b88debec26016de73185ff3ba5
Timo Stollenwerk <stollenwerk@kitconcept.com>
2017-07-27 12:25:47 +0200

    Add protractor to dev dependencies. Re-add transitions test. Use yarn on travis.

--- spec.js
+++ spec.js
@@ -13,12 +13,9 @@ describe("Worklow Editor", function() {
     expect(element.all(by.css(".state")).count()).toEqual(4);
   });
 
-/*
   it("should contain all four transitions", function() {
     browser.get("http://localhost:8080");
-    browser.pause()
-    expect(element.all(by.css(".jsplumb-connector")).count()).toEqual(4);
+    expect(element.all(by.css(".jtk-connector")).count()).toEqual(5);
   });
-*/
 
 });
